f6690dabaf570893fb80adbd61b77972a65bef48
JoaoSimino <joaovsmathiasdev@hotmail.com>
2025-07-15 08:01:42 -0300

    configurando componentes para enviar as requests para o YARP!

--- src/app/cliente/lista/lista.component.ts
+++ src/app/cliente/lista/lista.component.ts
@@ -17,7 +17,8 @@ export class ListaComponent implements OnInit{
   }
 
   carregarClientes() {
-    this.http.get<any[]>('http://localhost:5205/api/Cliente')
+    //this.http.get<any[]>('http://localhost:5205/api/Cliente')
+    this.http.get<any[]>('http://localhost:5062/api/Cliente')//apontando agora para o YARP!
       .subscribe(data => {
         this.clientes = data;
         this.cdr.detectChanges();  
